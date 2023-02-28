8a0c8240fe7bdc96052640b3fd1617cda0b8ed8f
Marcos <mrsilvatdas@gmail.com>
2023-02-27 22:14:49 -0300

    Initial coomit

---
+++ SignalRVotacao/Models/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRVotacao.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+        public virtual DbSet<Products> Products { get; set; }
+    }
+}
---
+++ SignalRVotacao/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SignalRVotacao.Models
+{
+    public class Products
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProdId { get; set; }
+        public string ProdName { get; set; }
+        public string Category { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int StockQty { get; set; }
+    }
+}
